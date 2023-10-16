33b52b217e2daec5ccc35763316df676bb31fb2a
Elisei <eliseykrapivin@gmail.com>
2023-10-16 16:55:48 +0600

    completely changed functions to classes

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views import View
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 
-def post_list(request):
+
+""" def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -23,8 +29,8 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
+            return #redirect('post_detail', pk=post.pk)
+   else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
@@ -40,9 +46,9 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form}) 
 
-@login_required
+ @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -61,4 +67,87 @@ def publish(self):
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post_list')
+    return redirect('post_list') """
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+
+    @login_required
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        if form.is_valid():
+            form.instance.author = self.request.user
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+
+    @login_required
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            form.instance.author = self.request.user
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDraftListView(ListView):
+    model = Post
+    template_name = 'blog/post_draft_list.html'
+    context_object_name = 'posts'
+
+    @login_required
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    
+class PostPublishView(View):
+    @login_required
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.publish()
+        return redirect('post_detail', pk=pk)
+
+class PostPublishView(UpdateView):
+    model = Post
+    @login_required
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        post.published_date = timezone.now()
+        post.save()
+        return self.get(request, *args, **kwargs)
+    
+class PostRemoveView(View):
+    @login_required
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect('post_list')
