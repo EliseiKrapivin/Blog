f6cb081fd7c9a22d9d1b5d88c86ef5b7a65982e3
Elisei <eliseykrapivin@gmail.com>
2023-10-18 13:15:03 +0600

    changed functions to classes, but without login_required

--- blog/urls.py
+++ blog/urls.py
@@ -1,32 +1,21 @@
 from django.urls import path
 from . import views
-from .views import PostListView
-from .views import PostDetailView
-from .views import PostCreateView
-from .views import PostUpdateView
-from .views import PostDraftListView
-from .views import PostPublishView
-from .views import PostRemoveView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    #path('', views.post_list, name='post_list'),
-    path('', PostListView.as_view(), name='post_list'),
-
-    #path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-
-    #path('post/new/', views.post_new, name='post_new'),
+    path('', PostListView.as_view(), name = 'post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_new'),
-
-    #path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/edit/<int:pk>/', PostUpdateView.as_view(), name='post_edit'),
-
-    #path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('drafts/', PostDraftListView.as_view(), name='post_draft_list'),
-
-    #path('post/<pk>/publish/', views.post_publish, name='post_publish'),
-    path('post/publish/<int:pk>/', PostPublishView.as_view(), name='post_publish'),
-
-    #path('post/<pk>/remove/', views.post_remove, name='post_remove'),
-    path('post/remove/<int:pk>/', PostRemoveView.as_view(), name='post_remove'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
+
+""" 
+    #path('', views.post_list, name='post_list'),
+    #path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #path('post/new/', views.post_new, name='post_new'),
+    #path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    #path('drafts/', views.post_draft_list, name='post_draft_list'),
+    #path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    #path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    """
--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,52 @@ from django.utils import timezone
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.views import View
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .forms import PostForm
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    template_name ='blog/post_edit.html'
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        feilds = form.save(commit=False)
+        feilds.author = self.request.user
+        feilds.published_date = timezone.now()
+        feilds.save()
+        return super().form_valid(form)
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'text']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('post_list')
 
 
 
-""" def post_list(request):
+
+
+"""
+ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -68,86 +105,3 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list') """
-
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
-
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_edit.html'
-
-    @login_required
-    def post(self, request, *args, **kwargs):
-        self.object = None
-        form = self.get_form()
-        if form.is_valid():
-            form.instance.author = self.request.user
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_edit.html'
-
-    @login_required
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = self.get_form()
-        if form.is_valid():
-            form.instance.author = self.request.user
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-class PostDraftListView(ListView):
-    model = Post
-    template_name = 'blog/post_draft_list.html'
-    context_object_name = 'posts'
-
-    @login_required
-    def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    
-class PostPublishView(View):
-    @login_required
-    def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        post.publish()
-        return redirect('post_detail', pk=pk)
-
-class PostPublishView(UpdateView):
-    model = Post
-    @login_required
-    def post(self, request, *args, **kwargs):
-        post = self.get_object()
-        post.published_date = timezone.now()
-        post.save()
-        return self.get(request, *args, **kwargs)
-    
-class PostRemoveView(View):
-    @login_required
-    def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        post.delete()
-        return redirect('post_list')
